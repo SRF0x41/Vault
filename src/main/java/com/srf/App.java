dc615e0502054976c7114dccddf515fafaea3aa9
Sergio Rodriguez <sergio.a.rodriguez@du.edu>
2025-02-03 22:53:15 -0700

    Fixed the ' issue escaping the query early

--- src/main/java/com/srf/App.java
+++ src/main/java/com/srf/App.java
@@ -12,8 +12,9 @@ public class App
             File dirSergio = new File("/Users/sergiorodriguez/Desktop");
             FileAnalyzer fa = new FileAnalyzer();
             searchAll.searchDir_toSQL(dirSergio, client, fa);
+            
+            
             //client.sendQuery("SELECT * FROM file_Index");
-
             //File testFile = new File("/Users/sergiorodriguez/Desktop/Differential Equations/ExamReview.pdf");
             //client.sendQuery_Update(fa.fileAtributes_toSQLQuery(testFile));
             //client.sendQuery_Query("SELECT * FROM file_Index");
