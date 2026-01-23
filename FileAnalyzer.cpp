a418119eae29d96a06f982aa12f2ec4eb4175430
Sergio Rodriguez <sergio.rodriguezflores26@gmail.com>
2026-01-23 00:43:35 -0700

    working on reading microsoft files

--- FileAnalyzer.cpp
+++ FileAnalyzer.cpp
@@ -1,51 +1,70 @@
 #include "FileAnalyzer.h"
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <string>
-
-// ---------------- Static initializer ----------------
-// get keywords stored into a hashmap
-struct FileAnalyzer::StaticInitializer {
-  StaticInitializer() {
-    std::cout << "Writing stop words to hashmap\n";
-    // You can put any code here that should run at program start
-    // Example: initialize some global keywords, log info, etc.
-  }
-};
-FileAnalyzer::StaticInitializer FileAnalyzer::initializer;
-
-/*
-describe file_index
-    -> ;
-+----------------+---------------+------+-----+---------+-------+
-| Field          | Type          | Null | Key | Default | Extra |
-+----------------+---------------+------+-----+---------+-------+
-| file_id        | int(11)       | NO   | PRI | NULL    |       |
-| file_size      | bigint(20)    | YES  |     | NULL    |       |
-| file_name      | varchar(255)  | YES  |     | NULL    |       |
-| file_extension | varchar(50)   | YES  |     | NULL    |       |
-| file_path      | varchar(1024) | YES  |     | NULL    |       |
-| file_keywords  | varchar(5000) | YES  |     | NULL    |       |
-+----------------+---------------+------+-----+---------+-------+
-6 rows in set (0.005 sec)
-
-*/
+#include <unordered_set>
 
 std::size_t FileAnalyzer::getSize(const std::string &path) {
-  return static_cast<std::size_t>(std::filesystem::file_size(path));
+  return std::filesystem::file_size(path);
 }
 
 std::string FileAnalyzer::getName(const std::string &path) {
-  return std::filesystem::path(path).filename();
+  return std::filesystem::path(path).filename().string();
 }
 
 std::string FileAnalyzer::getExt(const std::string &path) {
-  return std::filesystem::path(path).extension();
+  return std::filesystem::path(path).extension().string();
 }
 
-std::vector<std::string> getKeywords(const std::string &path) {}
+bool FileAnalyzer::isCompressed(const std::string &path) {
+  std::ifstream file(path, std::ios::binary);
+  if (!file)
+    return false;
 
-// Constructor
-FileAnalyzer::FileAnalyzer() {};
+  unsigned char header[4];
+  file.read(reinterpret_cast<char *>(header), 4);
 
-// Desctructor
-FileAnalyzer::~FileAnalyzer() {};
+  return header[0] == 0x50 && header[1] == 0x4B && header[2] == 0x03 &&
+         header[3] == 0x04;
+}
+
+bool FileAnalyzer::isMicrosoftCompressedXML(const std::string &path) {
+  static const std::unordered_set<std::string>
+      MicrosoftCompressedXML_format_extension = {".docx", ".xlsx", ".pptx",
+                                                 ".epub"};
+  return MicrosoftCompressedXML_format_extension.find(getExt(path)) !=
+         MicrosoftCompressedXML_format_extension.end();
+}
+
+bool FileAnalyzer::isPDF(const std::string &path) {
+  std::ifstream file(path, std::ios::binary);
+  if (!file)
+    return false;
+
+  char header[4];
+  file.read(header, 4);
+  return std::string(header, 4) == "%PDF";
+}
+
+bool FileAnalyzer::isRawText(const std::string &path) {
+  static const std::unordered_set<std::string> textExts = {
+      ".txt", ".md", ".log", ".csv", ".json", ".xml", ".yaml", ".ini"};
+
+  return textExts.find(getExt(path)) != textExts.end();
+}
+
+std::vector<std::string> FileAnalyzer::getKeywords(const std::string &path) {
+  std::ifstream input_file(path);
+  if (!input_file.is_open()) {
+    std::cerr << "Error opening stop words file." << "\n";
+    return {}; // Return empty vector on fail
+  }
+  if (isRawText(path)) {
+    std::string line;
+    while (input_file >> line) {
+      std::cout << line;
+    }
+  }
+  return {};
+}
